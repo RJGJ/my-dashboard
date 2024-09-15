9bd50d1f61a8af5e43cd01bbce92cf24aa437ec6
RJGJ <jaictinrj@gmail.com>
2024-09-15 18:35:05 +0800

    Added webapp

---
+++ webapp/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <section>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi ipsum
+          nesciunt reiciendis quos ad atque quibusdam illum quis quae, at sint
+          tempora, voluptatibus sapiente. Omnis similique quia ipsum laboriosam
+          iste.
+        </p>
+      </div>
+    </section>
+  </main>
+</template>
